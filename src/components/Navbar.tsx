523e7556ab961b2d99c381fae5295d951cbfca69
Linux Adona <linuxadona.17@proton.me>
2026-01-22 20:32:10 +0800

    Initial design for the Home page ongoing

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <>
+      <nav className="sticky top-0 bg-primary w-full h-16 z-50 shadow-2xl"></nav>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar.tsx";
+
+const Home = () => {
+  return (
+    <>
+      <div className="min-h-screen bg-gray-100">
+        <Navbar />
+      </div>
+    </>
+  );
+};
+
+export default Home;
